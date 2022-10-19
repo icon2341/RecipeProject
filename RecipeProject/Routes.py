4cb11025b01f5f93455350c857c50660cb8393cb
Brendan Battisti <brendan.battisti@gmail.com>
2022-10-19 15:27:23 -0400

    One more documentation thing

--- RecipeProject/Routes.py
+++ RecipeProject/Routes.py
@@ -1,17 +1,6 @@
 """
 Flask Backend to the recipe project web interface
 Author: Group 7 CSCI 320 01-02
-
-RUNNING INSTRUCTIONS:
-
-if in pycharm
-you can do "cntl + shift + f10" and it will attempt to run this file and create a run config, that will run the flask server
-note you will need to run
-
-COPY PASTE THIS INTO YOUR TERMINAL (ALT-F12):
-
-pip install flask-wtf flask wtforms flask_sqlalchemy SQLAlchemy
-
 """
 
 from flask import render_template, request, redirect
