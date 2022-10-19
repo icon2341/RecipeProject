0fc2c958571c1c008cab67a8f9bd50f2990a8387
Brendan Battisti <brendan.battisti@gmail.com>
2022-10-18 23:28:24 -0400

    Added in some stuff for user login

--- RecipeBackend.py
+++ RecipeBackend.py
@@ -3,23 +3,30 @@ Flask Backend to the recipe project web interface
 Author: Group 7 CSCI 320 01-02
 """
 
-
-
 import SQLInterface
 from flask import Flask, render_template, request, redirect
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
-
+#from flask_login import UserMixin, login_user
 from flask_sqlalchemy import SQLAlchemy
-
-
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '3f07e17a6aca41b3409e6e84af01dfd62ec479a6df127cc58485de51e2488383'
 
-#db = SQLAlchemy(app)
+username = "group7"
+password = "password"
+db_name = "db_name"
+app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://{username}:{password}@localhost:5432/{db_name}"
+
+db = SQLAlchemy(app)
+
+class Table(db.model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+
+
 
 # from flask import Flask, render_template
 # from flask_restful import Api, Resource, reqparse
@@ -36,6 +43,13 @@ app.config['SECRET_KEY'] = '3f07e17a6aca41b3409e6e84af01dfd62ec479a6df127cc58485
 # @app.route('/')
 # def tuna():
 #     return render_template("SignUp.html", func= getUserSignUpForm)
+#class User(db.Model, UserMixin):
+    #username
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Log In")
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
@@ -45,9 +59,18 @@ class RegistrationForm(FlaskForm):
 
     submit = SubmitField("Sign Up")
 
+@app.route("/wonk", methods=["GET"])
+def wonk():
+    posts = Table.query.all
+    return render_template("Home.html", posts=posts)
 
-@app.route("/Login")
+@app.route("/Login", methods=["GET", 'POST'])
 def Login():
+    form = LoginForm()
+    if request.method == "POST":
+
+       # login_user(user)
+        return redirect('/Home')
     return render_template("Login.html")
 
 
