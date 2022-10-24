e951f85e117c805373b8c8a5acb1bbd2f69756a9
JD Rears <jar6256@g.rit.edu>
2022-10-24 03:56:40 -0400

    Implemented program to parse through the data from a file and place it into the database

---
+++ RecipeProject/insert_data.py
@@ -0,0 +1,76 @@
+'''
+This program parses through data from a file and puts in into the database
+'''
+import pandas as pd
+import psycopg2
+import json
+from sshtunnel import SSHTunnelForwarder
+host_name = host='starbug.cs.rit.edu'
+dbname="p32001_07"
+port = 5432
+def __init__():
+        with open("PASSWORD.json") as pswd:
+            UserData = json.load(pswd)
+        db_name = "p32001_07"
+        server = SSHTunnelForwarder(('starbug.cs.rit.edu', 22),
+                                    ssh_username=UserData["USERNAME"],
+                                    ssh_password=UserData["PASSWORD"],
+                                    remote_bind_address=('localhost', 5432))
+
+        server.start()
+        params = {
+            'dbname': db_name,
+            'user': UserData["USERNAME"],
+            'password': UserData["PASSWORD"],
+            'host': 'localhost',
+            'port': server.local_bind_port,
+            "keepalives": 2,
+            "keepalives_idle": 30,
+            "keepalives_interval": 5,
+            "keepalives_count": 5
+        }
+
+        print("Server is up and running")
+        connection = psycopg2.connect(**params)
+        return connection
+
+def exists(curr, ruid):
+    query = ("""Select ruid FROM recipe2 WHERE ruid = %s""")
+    curr.execute(query, [ruid])
+    return curr.fetchone() is not None
+
+def update_db(curr, df):
+    tmp_df = pd.DataFrame(columns=['servings',  'description',
+                          'rating', 'cook_time', 'ruid',
+                                   'recipe_name', 'category', 'steps'])
+    for i, row in df.iterrows():
+        if exists(curr, row['ruid']):
+            '''maybe add update'''
+        else:
+            tmp_df = tmp_df.append(row)
+    return tmp_df
+
+def insert_into_table(curr, servings,  description,
+                          rating, cook_time, ruid,
+                                   recipe_name, category, steps):
+    query = ("""INSERT INTO recipe2 (servings,  description,
+                          rating, cook_time, ruid,
+                                   recipe_name, category, steps, difficulty)
+                        VALUES(%s, %s, %s, %s, %s, %s, %s, %s, 0);""")
+    row = servings, description,rating, cook_time, ruid, recipe_name, category, steps
+    curr.execute(query, row)
+
+def insert(curr, df):
+    for i, row in df.iterrows():
+        insert_into_table(curr, row['servings'], row['description'],
+                          row['rating'], row['cook_time'], row['ruid'],
+                                   row['recipe_name'], row['category'], row['steps'])
+con = __init__()
+curr = con.cursor()
+df = pd.read_csv('recipes.csv', usecols=['servings', 'description',
+                          'rating', 'cook_time', 'ruid',
+                                   'recipe_name', 'category', 'steps'], nrows=8) #remove nrows to parse all data
+tmp_df = update_db(curr, df)
+insert(curr, tmp_df)
+con.commit()
+print("Data added")
